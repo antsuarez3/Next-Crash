2f23921df4570cc3cdaed04d405abd7307f9997f
anthonysuarez <antsuarez305@gmail.com>
2023-10-05 16:59:20 -0500

    Create Header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './globals.css'
 import { Poppins } from 'next/font/google'
+import Header from './components/header'
 
 const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] })
 
@@ -15,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <main className='container'>{children}</main>
+        <main className='container'>
+          <Header /> {children}
+        </main>
       </body>
     </html>
   )
