ec05a9e7cb44e4e1bb09cf2dd2ea31187563f242
anthonysuarez <antsuarez305@gmail.com>
2023-10-05 16:34:26 -0500

    create layout, about pages

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About My Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+        reprehenderit provident? Cupiditate maiores laborum odit numquam sequi,
+        fuga eligendi iure illum explicabo pariatur at dicta, saepe autem cumque
+        voluptatibus blanditiis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+        reprehenderit provident? Cupiditate maiores laborum odit numquam sequi,
+        fuga eligendi iure illum explicabo pariatur at dicta, saepe autem cumque
+        voluptatibus blanditiis.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore totam
+        dolores beatae culpa dolorum corrupti incidunt alias ab porro ducimus
+        illum sit doloremque eos atque, dolorem animi perspiciatis sapiente cum.
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
