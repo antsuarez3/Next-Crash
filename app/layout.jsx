25395518f15f1d350f7a66968533c75f7fada1ec
anthonysuarez <antsuarez305@gmail.com>
2023-10-05 16:54:06 -0500

    set up fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'My Media',
@@ -14,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className='container'>{children}</main>
+      </body>
     </html>
   )
 }
